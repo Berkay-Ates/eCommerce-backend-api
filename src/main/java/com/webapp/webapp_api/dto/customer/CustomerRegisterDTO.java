1c9021365833112ed7d3e01d9cd8d297303c95db
Berkay Ates <atesberkay2356@gmail.com>
2025-07-04 23:05:29 +0300

    feat: add DTOs for customer and seller login and registration

---
+++ src/main/java/com/webapp/webapp_api/dto/customer/CustomerRegisterDTO.java
@@ -0,0 +1,26 @@
+package com.webapp.webapp_api.dto.customer;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomerRegisterDTO {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Surname is required")
+    private String surname;
+
+    @Email(message = "Valid email is required")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+}
