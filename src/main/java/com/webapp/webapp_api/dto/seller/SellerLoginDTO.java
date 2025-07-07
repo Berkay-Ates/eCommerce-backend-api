df45e4b9fc624da80c1b72eb996f1adae988865d
Berkay Ates <atesberkay2356@gmail.com>
2025-07-07 10:59:31 +0300

    refactor: SellerLoginDTO properties defined

--- src/main/java/com/webapp/webapp_api/dto/seller/SellerLoginDTO.java
+++ src/main/java/com/webapp/webapp_api/dto/seller/SellerLoginDTO.java
@@ -1,5 +1,18 @@
 package com.webapp.webapp_api.dto.seller;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SellerLoginDTO {
-    
+
+    @Email(message = "Valid email is required")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
 }
